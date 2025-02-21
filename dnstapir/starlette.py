033b0caeb336b4da43494e41a5476ed2a63d50b3
Jakob Schlyter <jakob@kirei.se>
2025-02-21 09:44:00 +0100

    Log client host/port (#65)
    * log remote host/port
    
    * simplify

--- dnstapir/starlette.py
+++ dnstapir/starlette.py
@@ -15,12 +15,22 @@ class LoggingMiddleware(BaseHTTPMiddleware):
         logger = structlog.get_logger()
         request_id = str(uuid.uuid4())
 
+        remote = (
+            {
+                "client_host": str(request.client.host),
+                "client_port": str(request.client.port),
+            }
+            if request.client
+            else {}
+        )
+
         with structlog.contextvars.bound_contextvars(request_id=request_id):
             logger.bind(
+                **remote,
                 method=request.method,
                 path=request.url.path,
             ).info(
-                f"Processing {request.method} request to {request.url.path}",
+                f"Processing {request.method} request from {request.client.host} to {request.url.path}",
             )
 
             request.state.start_time = time.perf_counter()
@@ -29,12 +39,13 @@ class LoggingMiddleware(BaseHTTPMiddleware):
             elapsed = time.perf_counter() - request.state.start_time
 
             logger.bind(
-                path=request.url.path,
+                **remote,
                 method=request.method,
+                path=request.url.path,
                 status_code=response.status_code,
                 elapsed=elapsed,
             ).info(
-                f"Processed {request.method} request to {request.url.path} in {elapsed:.3f} seconds",
+                f"Processed {request.method} request from {request.client.host} to {request.url.path} in {elapsed:.3f} seconds",
             )
 
             response.headers["X-Request-ID"] = request_id
