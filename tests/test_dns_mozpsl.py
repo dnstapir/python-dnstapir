d5de3402f664536417d1457acf711c12fd49b0a3
Jakob Schlyter <jakob@kirei.se>
2025-03-20 11:21:59 +0100

    mozpsl fixes from @morkrost (#24)
    * Order is important for longest-match... Also, typing.Self not in 3.9.
    
    * Fixed tests
    
    * Add dots to FQDNs
    
    * Tests, tests... now properly fixed, I hope.
    
    * we want typing
    
    ---------
    
    Co-authored-by: Mikael Kullberg <hula@catherd.se>

--- tests/test_dns_mozpsl.py
+++ tests/test_dns_mozpsl.py
@@ -9,13 +9,13 @@ def test_mozpsl():
     psl = PublicSuffixList()
     psl.load_psl_url(url=MOZ_PSL)
 
-    assert psl.coredomain("www.ck.") == ("www.ck", "")
-    assert psl.coredomain("www.something.gov.ck.") == ("something.gov.ck", "")
-    assert psl.coredomain("www.something.or.other.microsoft.com.") == ("microsoft.com", "")
-    assert psl.coredomain("www.something.or.other.microsoft.com.br.") == ("microsoft.com.br", "")
+    assert psl.coredomain("www.ck.") == ("ck.", "")
+    assert psl.coredomain("www.something.gov.ck.") == ("something.gov.ck.", "")
+    assert psl.coredomain("www.something.or.other.microsoft.com.") == ("microsoft.com.", "")
+    assert psl.coredomain("www.something.or.other.microsoft.com.br.") == ("microsoft.com.br.", "")
     assert psl.coredomain("www.something.emrstudio-prod.us-gov-east-1.amazonaws.com.") == (
-        "amazonaws.com",
-        "something.emrstudio-prod.us-gov-east-1.amazonaws.com",
+        "amazonaws.com.",
+        "something.emrstudio-prod.us-gov-east-1.amazonaws.com.",
     )
     assert psl.rdomain("com.amazonaws.us-gov-east-1.emrstudio-prod.www.something.emrstudio-prod") == (
         "com.amazonaws",
@@ -26,7 +26,7 @@ def test_mozpsl():
         psl.coredomain("local.")
 
     # IDN test
-    assert psl.coredomain("www.xn--mnchen-3ya.de.") == ("xn--mnchen-3ya.de", "")
+    assert psl.coredomain("www.xn--mnchen-3ya.de.") == ("xn--mnchen-3ya.de.", "")
 
     # Edge cases
     with pytest.raises(ValueError):
